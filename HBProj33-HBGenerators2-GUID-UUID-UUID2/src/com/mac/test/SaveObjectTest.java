971384da71c9be50e6889ae9604a573b8d88b128
Mac <Mac@Mac-PC>
2019-05-12 19:57:49 +0530

    first commit

---
+++ HBProj33-HBGenerators2-GUID-UUID-UUID2/src/com/mac/test/SaveObjectTest.java
@@ -0,0 +1,49 @@
+package com.mac.test;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.mac.entity.Customer;
+import com.mac.utility.HibernateUtil;
+
+public class SaveObjectTest {
+
+	public static void main(String[] args) {
+		Session ses = null;
+		Customer customer = null;
+		Transaction tx = null;
+		String idVal = null;
+		boolean flag = false;
+		ses = HibernateUtil.getSession();
+		customer = new Customer();
+		//customer.setCustId(9898);
+		customer.setCustName("jiya");
+		customer.setAddress("goa");
+		customer.setMobNo(8989989898l);
+		try {
+			//open session 
+			//begin transaction
+			tx = ses.beginTransaction();
+			idVal = (String) ses.save(customer);
+			System.out.println("Customer Id generate ::"+idVal);
+			flag = true;
+		}
+		
+		catch(Exception e) {
+			e.printStackTrace();
+			flag = false;
+		}
+		finally {
+			if(flag) {
+				tx.commit();
+				System.out.println("Object are Saved");
+			}
+			else {
+				tx.rollback();
+				System.out.println("Object are not Saved");
+			}
+			HibernateUtil.closeSession(ses);
+			HibernateUtil.closeSessionFactory();
+		}//finally
+	}//main
+}//class
